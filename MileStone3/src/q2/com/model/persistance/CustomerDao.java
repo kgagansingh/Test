c6f0f9ca00b290a2a74c26e711616f2a97ec9c5b
kgagansingh <87612295+kgagansingh@users.noreply.github.com>
2022-02-03 19:35:35 +0530

    Delete MileStone3 directory

--- MileStone3/src/q2/com/model/persistance/CustomerDao.java
+++
@@ -1,6 +0,0 @@
-package q2.com.model.persistance;
-
-public interface CustomerDao {
-	void addCustomer(Customer c);
-	Customer getById(int id);
-}
--- MileStone3/src/q2/com/model/service/CustomerDto.java
+++
@@ -1,8 +0,0 @@
-package q2.com.model.service;
-
-import q2.com.model.persistance.Customer;
-
-public interface CustomerDto {
-	void addCustomer(Customer c);
-	Customer getById(int id);
-}
