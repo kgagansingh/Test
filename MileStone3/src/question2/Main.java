892b573ab61fcb46de8906956d250b4fe3a35a67
kgagansingh <87612295+kgagansingh@users.noreply.github.com>
2022-02-03 16:35:07 +0530

    Add files via upload

---
+++ MileStone3/src/question2/Main.java
@@ -0,0 +1,16 @@
+package question2;
+
+//Q2. Write a program to add Customer and find customer by id  to a table
+//field of customer table :id, name,address, phone number, dob.
+//While coding consider following factors:
+//1. Implement DAO, DTO pattern
+//2. Using exception wrappring and rethrowning
+//3. Create connection factory, read connection details from a property file, in order to
+//have loose coupling
+
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
